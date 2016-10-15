d3568e22f8a3686f63ff12eca69db7f144bc1bd6
ubsan <npmazzuca@gmail.com>
2016-10-15 04:56:06 -0400

    THE FUCKING ASSEMBLER FUCKING WORKS

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -1,18 +1,37 @@
+#![allow(unused)]
+
 use std::env;
 use std::process;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{stderr, Write, Read};
+use std::io::{Write, Read};
+use std::fmt::{self, Debug};
 
-struct Register(u16);
-struct Memory(u16);
-struct Label(String);
+macro_rules! eprint {
+  ($($t:tt)*) => ({
+    use std::io::Write;
+    write!(::std::io::stderr(), $($t)*).expect("Writing to stderr failed")
+  })
+}
+macro_rules! eprintln {
+  ($($t:tt)*) => ({
+    use std::io::Write;
+    writeln!(::std::io::stderr(), $($t)*).expect("Writing to stderr failed")
+  })
+}
 
-enum Opcode {
+mod parser;
+
+pub struct Register(u16);
+pub struct Memory(u16);
+pub struct Label(String);
+
+pub enum Opcode {
   MoveImmediate(Register, u16),
   Move(Register, Memory),
   MoveDeref(Register, Memory),
-  Exchange(Register, Memory),
+  Load(Register, Memory),
+  Store(Register, Memory),
   Add(Register, Memory),
   Sub(Register, Memory),
   And(Register, Memory),
@@ -20,33 +39,116 @@ enum Opcode {
   Xor(Register, Memory),
   ShiftRight(Register, Memory),
   ShiftLeft(Register, Memory),
-  ShiftLogical(Register, Memory),
+  ShiftArithmetic(Register, Memory),
   JumpGreater(Register, Memory, Label),
   JumpLesser(Register, Memory, Label),
   JumpEqual(Register, Memory, Label),
-  Halt,
 }
 
-struct Labels(HashMap<String, Label>);
-struct Program(Vec<Opcode>);
-
-macro_rules! eprint {
-  ($($t:tt)*) => (
-    write!(stderr(), $($t)*).expect("Writing to stderr failed")
-  )
+impl Opcode {
+  fn size(&self) -> u16 {
+    use Opcode::*;
+    match *self {
+      MoveImmediate(..) | Move(..) | MoveDeref(..) | Load(..) | Store(..)
+      | Add(..) | Sub(..) | And(..) | Or(..) | Xor(..)
+      | ShiftRight(..) | ShiftLeft(..) | ShiftArithmetic(..) => {
+        2
+      }
+      JumpGreater(..) | JumpLesser(..) | JumpEqual(..) => {
+        3
+      }
+    }
+  }
 }
-macro_rules! eprintln {
-  ($($t:tt)*) => (
-    writeln!(stderr(), $($t)*).expect("Writing to stderr failed")
-  )
+
+impl Debug for Opcode {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    use Opcode::*;
+    match *self {
+      MoveImmediate(ref reg, ref imm) =>
+        write!(f, "MI 0x{:x}, 0x{:x}", reg.0, imm),
+      Move(ref reg, ref mem) =>
+        write!(f, "MV 0x{:x}, 0x{:x}", reg.0, mem.0),
+      MoveDeref(ref reg, ref mem) =>
+        write!(f, "MD 0x{:x}, 0x{:x}", reg.0, mem.0),
+      Load(ref reg, ref mem) =>
+        write!(f, "LD 0x{:x}, 0x{:x}", reg.0, mem.0),
+      Store(ref reg, ref mem) =>
+        write!(f, "ST 0x{:x}, 0x{:x}", reg.0, mem.0),
+      Add(ref reg, ref mem) =>
+        write!(f, "AD 0x{:x}, 0x{:x}", reg.0, mem.0),
+      Sub(ref reg, ref mem) =>
+        write!(f, "SB 0x{:x}, 0x{:x}", reg.0, mem.0),
+      And(ref reg, ref mem) =>
+        write!(f, "ND 0x{:x}, 0x{:x}", reg.0, mem.0),
+      Or(ref reg, ref mem) =>
+        write!(f, "OR 0x{:x}, 0x{:x}", reg.0, mem.0),
+      Xor(ref reg, ref mem) =>
+        write!(f, "XR 0x{:x}, 0x{:x}", reg.0, mem.0),
+      ShiftRight(ref reg, ref mem) =>
+        write!(f, "SR 0x{:x}, 0x{:x}", reg.0, mem.0),
+      ShiftLeft(ref reg, ref mem) =>
+        write!(f, "SL 0x{:x}, 0x{:x}", reg.0, mem.0),
+      ShiftArithmetic(ref reg, ref mem) =>
+        write!(f, "SA 0x{:x}, 0x{:x}", reg.0, mem.0),
+      JumpGreater(ref reg, ref mem, ref label) =>
+        write!(f, "JG 0x{:x}, 0x{:x}, {}", reg.0, mem.0, label.0),
+      JumpLesser(ref reg, ref mem, ref label) =>
+        write!(f, "JL 0x{:x}, 0x{:x}, {}", reg.0, mem.0, label.0),
+      JumpEqual(ref reg, ref mem, ref label) =>
+        write!(f, "JQ 0x{:x}, 0x{:x}, {}", reg.0, mem.0, label.0),
+    }
+  }
+}
+
+struct Program {
+  labels: HashMap<String, u16>,
+  program: Vec<(Opcode, u16)>,
+}
+
+impl Program {
+  fn new(input: Vec<u8>) -> Self {
+    let mut this = Program {
+      labels: HashMap::new(),
+      program: Vec::new(),
+    };
+
+    let mut offset = 0;
+    for op_or_label in parser::Parser::new(input) {
+      match op_or_label {
+        parser::OpOrLabel::Op(op) => {
+          let tmp = op.size();
+          this.program.push((op, offset));
+          offset += tmp;
+        }
+        parser::OpOrLabel::Label(label, idx) => {
+          this.labels.insert(label, idx);
+        }
+      }
+    }
+
+    this
+  }
 }
 
 fn main() {
   let args = env::args_os().collect::<Vec<_>>();
-  let filename = match args.get(1) {
+  let inpfilename = match args.get(1) {
     Some(f) => f,
     None => {
-      eprintln!("usage: {} [filename]",
+      eprintln!("usage: {} filename -o output",
+        args
+          .get(0)
+          .map(|own| own.to_string_lossy())
+          .unwrap_or("program".into()),
+      );
+      process::exit(1);
+    }
+  };
+  let outfilename = match args.get(3) {
+    Some(f) => f,
+    None => {
+      eprintln!("usage: {} filename -o output",
         args
           .get(0)
           .map(|own| own.to_string_lossy())
@@ -56,28 +158,109 @@ fn main() {
     }
   };
 
-  let mut file = match File::open(filename) {
+  let mut inp = match File::open(inpfilename) {
     Ok(file) => file,
     Err(e) => {
       eprintln!(
-        "Failed to open file: `{}'\nError: {}", filename.to_string_lossy(), e,
+        "Failed to open input file: `{}'\nError: {}",
+        inpfilename.to_string_lossy(),
+        e,
+      );
+      process::exit(1);
+    }
+  };
+  let mut out = match File::create(outfilename) {
+    Ok(file) => file,
+    Err(e) => {
+      eprintln!(
+        "Failed to open output file: `{}'\nError: {}",
+        outfilename.to_string_lossy(),
+        e,
       );
       process::exit(1);
     }
   };
 
   let mut bytes = Vec::new();
-  match file.read_to_end(&mut bytes) {
+  match inp.read_to_end(&mut bytes) {
     Ok(_) => {},
     Err(e) => {
       eprintln!(
-        "Failed to read file: `{}'\nError: {}", filename.to_string_lossy(), e,
+        "Failed to read file: `{}'\nError: {}",
+        inpfilename.to_string_lossy(),
+        e,
       );
       process::exit(1);
     }
   }
 
-  unsafe {
-    println!("{}", String::from_utf8_unchecked(bytes));
+  let program = Program::new(bytes);
+  let labels = program.labels;
+  for op in program.program {
+    use Opcode::*;
+    let offset = op.1;
+
+    fn write(out: &mut File, to_write: &[u16]) {
+      let res = out.write_all(
+        unsafe {
+          std::slice::from_raw_parts(
+            to_write.as_ptr() as *const u8,
+            to_write.len() * 2,
+          )
+        }
+      );
+      match res {
+        Ok(_) => {}
+        Err(e) => {
+          eprintln!("Error while writing: {}", e);
+          process::exit(1);
+        }
+      }
+    };
+    let mut jump = |
+      out: &mut File, lge: i8, reg: Register, mem: Memory, label: Label
+    | {
+      let label = match labels.get(&label.0) {
+        Some(&l) => l,
+        None => {
+          if (label.0).is_empty() {
+            offset
+          } else {
+            eprintln!("Label not found: {}", label.0);
+            process::exit(1);
+          }
+        }
+      };
+
+      let jr = label.wrapping_sub(offset);
+
+      if lge > 0 {
+        write(out, &[(0xD << 12) | reg.0, mem.0, jr]);
+      } else if lge < 0 {
+        write(out, &[(0xE << 12) | reg.0, mem.0, jr]);
+      } else {
+        write(out, &[(0xF << 12) | reg.0, mem.0, jr]);
+      }
+    };
+
+    match op.0 {
+      MoveImmediate(reg, imm) => write(&mut out, &[(0x0 << 12) | reg.0, imm]),
+      Move(reg, mem) => write(&mut out, &[(0x1 << 12) | reg.0, mem.0]),
+      MoveDeref(reg, mem) => write(&mut out, &[(0x2 << 12) | reg.0, mem.0]),
+      Load(reg, mem) => write(&mut out, &[(0x3 << 12) | reg.0, mem.0]),
+      Store(reg, mem) => write(&mut out, &[(0x4 << 12) | reg.0, mem.0]),
+      Add(reg, mem) => write(&mut out, &[(0x5 << 12) | reg.0, mem.0]),
+      Sub(reg, mem) => write(&mut out, &[(0x6 << 12) | reg.0, mem.0]),
+      And(reg, mem) => write(&mut out, &[(0x7 << 12) | reg.0, mem.0]),
+      Or(reg, mem) => write(&mut out, &[(0x8 << 12) | reg.0, mem.0]),
+      Xor(reg, mem) => write(&mut out, &[(0x9 << 12) | reg.0, mem.0]),
+      ShiftRight(reg, mem) => write(&mut out, &[(0xA << 12) | reg.0, mem.0]),
+      ShiftLeft(reg, mem) => write(&mut out, &[(0xB << 12) | reg.0, mem.0]),
+      ShiftArithmetic(reg, mem) =>
+        write(&mut out, &[(0xC << 12) | reg.0, mem.0]),
+      JumpGreater(reg, mem, label) => jump(&mut out, 1, reg, mem, label),
+      JumpLesser(reg, mem, label) => jump(&mut out, 1, reg, mem, label),
+      JumpEqual(reg, mem, label) => jump(&mut out, 1, reg, mem, label),
+    }
   }
 }
---
+++ assembler/src/parser.rs
@@ -0,0 +1,321 @@
+use std::process;
+use Opcode;
+
+pub enum OpOrLabel {
+  Op(Opcode),
+  Label(String, u16),
+}
+
+pub struct Parser {
+  lexer: Lexer,
+  offset: u16,
+}
+
+impl Parser {
+  pub fn new(input: Vec<u8>) -> Self {
+    Parser { lexer: Lexer::new(input), offset: 0 }
+  }
+
+  fn get_rm(&mut self) -> ::Register {
+    use self::Token::*;
+    let ret = self.get_mem();
+    if ret.0 >= 0x1000 {
+      eprintln!("Overflowing literal for register memory: 0x{:x}", ret.0);
+      process::exit(1);
+    }
+    ::Register(ret.0)
+  }
+  fn get_mem(&mut self) -> ::Memory {
+    use self::Token::*;
+    match self.lexer.get_token() {
+      Some(Ident(ident)) => {
+        match &*ident {
+          "IP" => ::Memory(0x0),
+          "SP" => ::Memory(0x1),
+          "BP" => ::Memory(0x2),
+          "SC" => ::Memory(0x3),
+          reg => {
+            eprintln!("Unrecognized register memory name: {}", reg);
+            process::exit(1);
+          }
+        }
+      }
+      Some(Number(n)) => {
+        ::Memory(n)
+      }
+      Some(Label(label)) => {
+        eprintln!("Label not supported here: {}", label);
+        process::exit(1);
+      }
+      None => {
+        eprintln!("Unexpected EOF");
+        process::exit(1);
+      }
+    }
+  }
+  fn get_label(&mut self) -> ::Label {
+    use self::Token::*;
+    match self.lexer.get_token() {
+      Some(Label(label)) => {
+        eprintln!("Label definition not expected: `{}'", label);
+        process::exit(1);
+      }
+      Some(Ident(ident)) => {
+        ::Label(ident)
+      }
+      Some(Number(n)) => {
+        eprintln!("Label expected, found number: 0x{:x}", n);
+        process::exit(1);
+      }
+      None => {
+        eprintln!("Unexpected EOF");
+        process::exit(1);
+      }
+    }
+  }
+}
+
+impl Iterator for Parser {
+  type Item = OpOrLabel;
+
+  fn next(&mut self) -> Option<OpOrLabel> {
+    use self::Token::*;
+    match self.lexer.get_token() {
+      Some(Label(label)) => {
+        Some(OpOrLabel::Label(label, self.offset))
+      }
+      Some(Ident(ident)) => {
+        Some(OpOrLabel::Op(
+          match &*ident {
+            "MI" => {
+              Opcode::MoveImmediate(self.get_rm(), self.get_mem().0)
+            }
+            "MV" => {
+              Opcode::Move(self.get_rm(), self.get_mem())
+            }
+            "MD" => {
+              Opcode::MoveDeref(self.get_rm(), self.get_mem())
+            }
+            "LD" => {
+              Opcode::Load(self.get_rm(), self.get_mem())
+            }
+            "ST" => {
+              Opcode::Store(self.get_rm(), self.get_mem())
+            }
+            "AD" => {
+              Opcode::Add(self.get_rm(), self.get_mem())
+            }
+            "SB" => {
+              Opcode::Sub(self.get_rm(), self.get_mem())
+            }
+            "ND" => {
+              Opcode::And(self.get_rm(), self.get_mem())
+            }
+            "OR" => {
+              Opcode::Or(self.get_rm(), self.get_mem())
+            }
+            "XR" => {
+              Opcode::Xor(self.get_rm(), self.get_mem())
+            }
+            "SR" => {
+              Opcode::ShiftRight(self.get_rm(), self.get_mem())
+            }
+            "SL" => {
+              Opcode::ShiftLeft(self.get_rm(), self.get_mem())
+            }
+            "SA" => {
+              Opcode::ShiftArithmetic(self.get_rm(), self.get_mem())
+            }
+            "JG" => {
+              Opcode::JumpGreater(
+                self.get_rm(), self.get_mem(), self.get_label(),
+              )
+            }
+            "JL" => {
+              Opcode::JumpLesser(
+                self.get_rm(), self.get_mem(), self.get_label(),
+              )
+            }
+            "JQ" => {
+              Opcode::JumpEqual(
+                self.get_rm(), self.get_mem(), self.get_label(),
+              )
+            }
+            "HF" => {
+              Opcode::JumpEqual(
+                ::Register(0), ::Memory(0), ::Label(String::new()),
+              )
+            }
+            op => {
+              eprintln!("Unsupported op code: {}", op);
+              process::exit(1);
+            }
+          }
+        ))
+      }
+      Some(Number(n)) => {
+        eprintln!("Unexpected number literal: 0x{:x}", n);
+        process::exit(1);
+      }
+      None => None,
+    }
+  }
+}
+
+enum Token {
+  Label(String),
+  Ident(String),
+  Number(u16),
+}
+
+struct Lexer {
+  input: Vec<u8>,
+  idx: usize,
+}
+impl Lexer {
+  fn new(input: Vec<u8>) -> Self {
+    Lexer {
+      input: input,
+      idx: 0
+    }
+  }
+
+  fn peek_char(&self) -> Option<u8> {
+    fn toupper(c: u8) -> u8 {
+      if c >= b'a' && c <= b'z' {
+        c - (b'a' - b'A')
+      } else {
+        c
+      }
+    }
+    self.input.get(self.idx).map(|&c| toupper(c))
+  }
+
+  fn get_char(&mut self) -> Option<u8> {
+    match self.peek_char() {
+      Some(c) => {
+        self.idx += 1;
+        Some(c)
+      }
+      None => None,
+    }
+  }
+
+  fn get_token(&mut self) -> Option<Token> {
+    fn is_space(c: u8) -> bool {
+      c == b' ' || c == b'\t' || c == b'\n' ||
+        c == 0x0b || c == 0x0c  || c == b'\r'
+    }
+    fn is_alpha(c: u8) -> bool {
+      (c >= b'a' && c <= b'z') || (c >= b'A' && c <= b'Z')
+    }
+    fn is_num(c: u8) -> bool {
+      c >= b'0' && c <= b'9'
+    }
+    fn is_alnum(c: u8) -> bool {
+      is_alpha(c) || is_num(c)
+    }
+    fn is_allowed(c: u8, base: u32) -> bool {
+      match base {
+        2 => c >= b'0' && c < b'2',
+        8 => c >= b'0' && c < b'8',
+        10 => is_num(c),
+        16 => is_num(c) || (c >= b'A' && c <= b'F'),
+        _ => process::exit(192),
+      }
+    }
+    match self.get_char() {
+      Some(ch) if is_space(ch) || ch == b',' => {
+        while let Some(c) = self.peek_char() {
+          if is_space(c) { self.get_char(); }
+          else { break; }
+        }
+        self.get_token()
+      }
+      Some(ch) if is_alpha(ch) => {
+        let mut ret = Vec::new();
+        ret.push(ch);
+        while let Some(c) = self.peek_char() {
+          if is_alnum(c) {
+            self.get_char();
+            ret.push(c);
+          } else if c == b':' {
+            self.get_char();
+            return Some(Token::Label(String::from_utf8(ret).unwrap()));
+          } else {
+            break;
+          }
+        }
+        Some(Token::Ident(String::from_utf8(ret).unwrap()))
+      }
+      Some(ch) if is_num(ch) => {
+        let mut base = 10;
+        let mut ret = Vec::new();
+        if ch == b'0' {
+          let peek = self.peek_char().unwrap_or(b'D');
+          if is_num(peek) {
+            while let Some(ch) = self.peek_char() {
+              if ch == b'0' { self.get_char(); }
+              else { break; }
+            }
+          } else if peek == b'B' {
+            base = 2;
+            self.get_char();
+          } else if peek == b'O' {
+            base = 8;
+            self.get_char();
+          } else if peek == b'D' {
+            base = 10;
+            self.get_char();
+          } else if peek == b'X' {
+            base = 16;
+            self.get_char();
+          } else {
+            eprintln!(
+              "Unsupported character in a number literal: {}", peek as char
+            );
+            process::exit(1);
+          }
+        }
+
+        while let Some(ch) = self.peek_char() {
+          if is_allowed(ch, base as u32) {
+            self.get_char();
+            ret.push(ch);
+          } else if is_alpha(ch) {
+            eprintln!(
+              "Unsupported character in a number literal: {}", ch as char
+            );
+            process::exit(1);
+          } else {
+            break;
+          }
+        }
+        let mut acc = 0u16;
+        for el in &ret {
+          let add = if is_alpha(*el) {
+            el - b'A' + 10
+          } else {
+            el - b'0'
+          } as u16;
+          acc = match acc.checked_mul(base).and_then(|a| a.checked_add(add)) {
+              Some(a) => a,
+              None => {
+                eprintln!(
+                  "Attempted to write an overflowing number literal: {}",
+                  unsafe{::std::str::from_utf8_unchecked(&ret)},
+                );
+                process::exit(1);
+              }
+            }
+        }
+        Some(Token::Number(acc))
+      }
+      Some(ch) => {
+        eprintln!("Unsupported character: `{}' ({})", ch as char, ch);
+        process::exit(1);
+      }
+      None => None,
+    }
+  }
+}
