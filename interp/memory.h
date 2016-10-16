b1b6e1ecc67cf56022a351bde1056aa750ecbd97
Josh Hofing <joshhofing@gmail.com>
2016-10-16 18:03:16 -0400

    Add supress_redraw flag

--- interp/memory.h
+++ interp/memory.h
@@ -10,7 +10,8 @@ typedef uint16_t mem_t;
 #define DISPLAY_REGION_SIZE (DISPLAY_REGION_DIMENSIONS * DISPLAY_REGION_DIMENSIONS)
 #define DISPLAY_REGION_START 0x100
 #define DISPLAY_REGION_END (DISPLAY_REGION_START + DISPLAY_REGION_SIZE)
-#define DISPLAY_CONTROL_REG_LOC (DISPLAY_REGION_START-1)
+#define DISPLAY_COORD_REG_LOC (DISPLAY_REGION_START-1)
+#define DISPLAY_CONTROL_REG_LOC (DISPLAY_REGION_START-2)
 
 #define PRINT_LOC 0x5
 
@@ -32,8 +33,11 @@ public:
     memory() : disp(), mem{0} {};
     // access methods
     mem_t& operator[](size_t idx) {
-        if (DISPLAY_CONTROL_REG_LOC == idx) {
+        if (DISPLAY_COORD_REG_LOC == idx) {
             return disp.disp_coords;
+        }
+        if (DISPLAY_CONTROL_REG_LOC == idx) {
+            return redraw_supressed;
         }
         if (DISPLAY_REGION_START <= idx && idx < DISPLAY_REGION_END) {
             must_update_display = true;
@@ -54,7 +58,7 @@ public:
     }
 
     void flush_changes() {
-        if (must_update_display) {
+        if (must_update_display && !redraw_supressed) {
             disp.redraw();
             must_update_display = false;
         }
@@ -65,6 +69,7 @@ public:
     }
 
     bool must_print, must_update_display;
+    mem_t redraw_supressed; // don't redraw until this is 0.
     display disp;
     mem_t mem[1<<15] = {0}; // lookups that don't hit mmapped regions hit this
 };
