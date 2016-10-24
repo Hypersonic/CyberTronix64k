1da7d4f4838b623f11e5619464e5069fd3290096
Josh Hofing <joshhofing@gmail.com>
2016-10-24 17:20:49 -0400

    Move character I/O to 0x200 and above

---
+++ football/get_num.asm
@@ -0,0 +1,17 @@
+equ IN  0x201
+equ ARG1  0x40
+
+JI get_digit_input
+
+get_digit_input:
+    MV ARG1, IN
+    ; bounds checks
+    MI SC, '0'
+    JL ARG1, SC, get_digit_input__err
+    MI SC, '9'
+    JG ARG1, SC, get_digit_input__err
+    SBI ARG1, '0'
+    HF ; TODO: ret
+
+get_digit_input__err:
+    HF
