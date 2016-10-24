90397b64565144befa74320bc32013434e1bdd36
Josh Hofing <joshhofing@gmail.com>
2016-10-24 17:22:46 -0400

    Unswap get/put char locs

--- interp/memory.h
+++ interp/memory.h
@@ -4,8 +4,8 @@
 
 typedef uint16_t mem_t;
 
-#define GETC_LOC 0x200
-#define PUTC_LOC 0x201
+#define PUTC_LOC 0x200
+#define GETC_LOC 0x201
 
 
 class memory {
@@ -13,12 +13,12 @@ class memory {
         memory() : mem{0} {};
         // access methods
         mem_t& operator[](size_t idx) {
-            if (idx == GETC_LOC) {
-                mem[idx] = mem_t(getchar());
-                return mem[idx];
-            }
             if (idx == PUTC_LOC) {
                 must_print = true;
+                return mem[idx];
+            }
+            if (idx == GETC_LOC) {
+                mem[idx] = mem_t(getchar());
                 return mem[idx];
             }
             return mem[idx];
