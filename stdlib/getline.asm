16d9ac65f2dcfac2ce9b493a46503fc6861e31b3
ubsan <npmazzuca@gmail.com>
2016-11-02 17:07:21 -0400

    Add println.asm, optimize getline.asm

--- stdlib/getline.asm
+++ stdlib/getline.asm
@@ -35,29 +35,30 @@ EQU S09 0x49
 
 ; takes (ptr, len):
 ;     (s00, s01)
+EQU __a_getline_capacity s01
 ; returns (ptr, len):
 ;     (s00, s01)
-EQU __a_getline_ptr s00
-EQU __a_getline_capacity s01
+EQU __ar_getline_ptr s00
+EQU __r_getline_len s01
 ; stack variables:
 EQU __v_getline_start s02
 EQU __v_getline_end s03
 EQU __v_getline_c s04
-EQU __v_getline_len s05
-EQU __v_getline_nl s06
+EQU __v_getline_nl s05
+EQU __v_getline_one s06
 getline:
 	mi __v_getline_nl, '\n'
-  mv __v_getline_start, __a_getline_ptr
-  mv __v_getline_end, __a_getline_ptr
+  mv __v_getline_start, __ar_getline_ptr
+  mv __v_getline_end, __ar_getline_ptr
   ad __v_getline_end, __a_getline_capacity
   jq __v_getline_start, __v_getline_end, __l_getline_loop_break
 		__v_getline_loop_start:
 			mv __v_getline_c, STDIN
 			jq __v_getline_c, __v_getline_nl, __l_getline_loop_break
 			ld __v_getline_start, __v_getline_c
-			inc __v_getline_start
+			ad __v_getline_start, __v_getline_one
 			ji __v_getline_loop_start
 		__l_getline_loop_break:
-  mv __v_getline_len, __v_getline_end
-  sb __v_getline_len, __a_getline_ptr
+  mv __r_getline_len, __v_getline_end
+  sb __r_getline_len, __ar_getline_ptr
   ret
