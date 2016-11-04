a9090de7fc080d2ea08de1b2a28d94e13f08c8d9
ubsan <npmazzuca@gmail.com>
2016-11-03 20:58:44 -0400

    fix getline

--- tests/std/getline.asm
+++ tests/std/getline.asm
@@ -23,32 +23,35 @@ global getline
 #   return { ptr, len };
 # }
 
-; takes (ptr, len):
-;     (s00, s01)
-EQU __a_getline_capacity s01
-; returns (ptr, len):
-;     (s00, s01)
-EQU __ar_getline_ptr s00
-EQU __r_getline_len s01
-; stack variables:
-EQU __v_getline_start s02
-EQU __v_getline_end s03
-EQU __v_getline_c s04
-EQU __v_getline_nl s05
-EQU __v_getline_one s06
+equ __ar_getline_ptr s00
+equ __a_getline_capacity s01
+equ __r_getline_len s01
+
+equ __c_getline_lf s02
+equ __c_getline_1 s03
+
+equ __v_getline_start s04
+equ __v_getline_end s05
+equ __v_getline_c s06
+
 getline:
-	mi __v_getline_nl, '\n'
+	mi __c_getline_nl, '\n'
+	mi __c_getline_1, 1
+
   mv __v_getline_start, __ar_getline_ptr
   mv __v_getline_end, __ar_getline_ptr
   ad __v_getline_end, __a_getline_capacity
-  jq __v_getline_start, __v_getline_end, __l_getline_loop_break
+
+	__v_getline_loop_test:
+  jq __v_getline_start, __v_getline_end, __l_getline_loop_end
 		__v_getline_loop_start:
 			mv __v_getline_c, STDIN
-			jq __v_getline_c, __v_getline_nl, __l_getline_loop_break
+			jq __v_getline_c, __v_getline_nl, __l_getline_loop_end
 			ld __v_getline_start, __v_getline_c
-			ad __v_getline_start, __v_getline_one
-			ji __v_getline_loop_start
-		__l_getline_loop_break:
+			ad __v_getline_start, __v_getline_1
+			ji __v_getline_loop_test
+		__l_getline_loop_end:
+
   mv __r_getline_len, __v_getline_end
   sb __r_getline_len, __ar_getline_ptr
   ret
