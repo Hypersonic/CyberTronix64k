3b3892ab5c9d6a4bda850402426d2a456d31838f
ubsan <npmazzuca@gmail.com>
2016-11-08 23:34:39 -0500

    docs, stuff in main

--- football/main.asm
+++ football/main.asm
@@ -1,4 +1,5 @@
 ji start
+
 import std.constants
 import std.getline
 import std.println
@@ -102,20 +103,20 @@ data input_string_len
 ; jump table, length 15
 jump_table:
 entry_0:  data jt0
-entry_1:  data jt0
-entry_2:  data jt0
-entry_3:  data jt0
-entry_4:  data jt0
-entry_5:  data jt0
-entry_6:  data jt0
-entry_7:  data jt0
-entry_8:  data jt0
-entry_9:  data jt0
-entry_10: data jt0
-entry_11: data jt0
-entry_12: data jt0
-entry_13: data jt0
-entry_14: data jt0
+entry_1:  data jt1
+entry_2:  data jt2
+entry_3:  data jt3
+entry_4:  data jt4
+entry_5:  data jt5
+entry_6:  data jt6
+entry_7:  data jt7
+entry_8:  data jt8
+entry_9:  data jt9
+entry_10: data jt10
+entry_11: data jt11
+entry_12: data jt12
+entry_13: data jt13
+entry_14: data jt14
 
 ; a struct password (importantly, directly after jumptable)
 password:
@@ -133,9 +134,158 @@ jt0:
     call println
     hf
 
+jt_fail:
+    mi s00, fail_string
+    mi s01, fail_string_len
+    call println
+    hf
+
+jt1:
+    mi s00, target1_string
+    mi s01, target1_len
+    call println
+    hf
+jt2:
+    mi s00, target2_string
+    mi s01, target2_len
+    call println
+    hf
+jt3:
+    mv r00, s02
+    mv r01, s03
+    mi r02, 0
+    mi s02, target3_user
+    mi s03, target3_user_len
+    call memeq
+    jq s00, r02, jt_fail
+
+    mv s00, r00
+    mv s01, r01
+    mi s02, target3_pw
+    mi s03, target3_pw_len
+    call memeq
+    jq s00, r02, jt_fail
+
+    mi s00, target3_string
+    mi s01, target3_len
+    call println
+    hf
+
+jt4:
+    mi s00, target4_string
+    mi s01, target4_len
+    call println
+    hf
+jt5:
+    mi s00, target5_string
+    mi s01, target5_len
+    call println
+    hf
+jt6:
+    mi s00, target6_string
+    mi s01, target6_len
+    call println
+    hf
+jt7:
+    mi s00, target7_string
+    mi s01, target7_len
+    call println
+    hf
+jt8:
+    mi s00, target8_string
+    mi s01, target8_len
+    call println
+    hf
+jt9:
+    mi s00, target9_string
+    mi s01, target9_len
+    call println
+    hf
+jt10:
+    mi s00, target10_string
+    mi s01, target10_len
+    call println
+    hf
+jt11:
+    mi s00, target11_string
+    mi s01, target11_len
+    call println
+    hf
+jt12:
+    mv r00, s02
+    mv r01, s03
+    mi r02, 0
+    mi s02, target12_user
+    mi s03, target12_user_len
+    call memeq
+    jq s00, r02, jt_fail
+
+    mv s00, r00
+    mv s01, r01
+    mi s02, target12_pw
+    mi s03, target12_pw_len
+    call memeq
+    jq s00, r02, jt_fail
+
+    mi s00, target12_string
+    mi s01, target12_len
+    call println
+    hf
+jt13:
+    mi s00, target13_string
+    mi s01, target13_len
+    call println
+    hf
+jt14:
+    mi s00, target14_string
+    mi s01, target14_len
+    call println
+    hf
+
+
 
 
 tmp_junk_target_s:
 data "DEFINE THIS JUMP TARGET HOMIE"
-
 equ tmp_junk_target_s_len 29 
+
+fail_string: data "That username and password are not recognized."
+equ fail_string_len, 46
+
+target1_string: data "A C program is like a fast dance on a newly waxed dance floor by people carrying razors."
+equ target1_string_len, 88
+target2_string: data "FORTRAN is not a flower but a weed - it is hardy, occasionally blooms, and grows in every computer."
+equ target2_string_len, 99
+target3_string: data "C++: an octopus made by nailing extra legs onto a dog."
+equ target3_string_len, 54
+target3_user: data "ubsan"
+equ target3_user_len, 5
+target3_pw: data "RustIsTheBomb"
+equ target3_pw_len, 13
+
+target5_string: data "Perl - The only language that looks the same before and after RSA encryption."
+equ target5_string_len, 77
+target6_string: data "I had a problem, so I thought I'd use Rust. Now I have &'a &'b mut Problem that I cannot move out of a borrowed context."
+equ target6_string_len, 120
+target7_string: data "Go is not a good language. It's not bad; it's just not good."
+equ target7_string_len, 60
+target8_string: data "If Java had true garbage collection, most programs would delete themselves upon execution."
+equ target8_string_len, 90
+target9_string: data "PHP is built to keep chugging along at all costs. When faced with either doing something nonsensical or aborting with an error, it will do something nonsensical."
+equ target9_string_len, 161 
+target10_string: data "Lisp is not an acceptable LISP. Not for any value of Lisp."
+equ target10_string_len, 58
+target11_string: data "JavaScript is built on some very good ideas and a few very bad ones."
+equ target11_string_len, 68
+
+target12_string: data "Fuck you Trump. How the fuck?"
+equ target12_string_len, 29
+target12_user: data "trump"
+equ target12_user_len, 5
+target12_pw: data "ImAGiantFuckingAsshole"
+equ target12_pw_len, 13
+
+target13_string: data "The use of COBOL cripples the mind; its teaching should therefore be regarded as a criminal offense."
+equ target13_string_len, 100
+target14_string: data "Python's a drop-in replacement for BASIC in the sense that Optimus Prime is a drop-in replacement for a truck."
+equ target14_string_len, 110
